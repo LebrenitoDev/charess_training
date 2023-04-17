aac196055728e4ecc829c84bdddca5e396f06465
Schneider Aristilde <schneider.aristilde.as@gmail.com>
2023-04-17 15:29:17 -0400

    Added autologout features after some time with no activity

---
+++ src/main/webapp/src/app/core/service/auto-log-out.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AutoLogOutService {
+
+  private readonly MINUTES_UNITL_AUTO_LOGOUT = 3;
+  private readonly CHECK_INTERVAL = 10000;
+  private readonly STORE_KEY = 'lastAction';
+
+   constructor(private router: Router) {
+    this.check();
+    this.initListener();
+    this.initInterval();
+    localStorage.setItem(this.STORE_KEY, Date.now().toString());
+  }
+
+  public getLastAction() {
+    return parseInt(localStorage.getItem(this.STORE_KEY));
+  }
+
+  public setLastAction(lastAction: number) {
+    localStorage.setItem(this.STORE_KEY, lastAction.toString());
+  }
+
+  private initListener() {
+    document.body.addEventListener('click', () => this.reset());
+    document.body.addEventListener('mousemove', () => this.reset());
+    document.body.addEventListener('keydown', () => this.reset());
+    document.body.addEventListener('keyup', () => this.reset());
+    document.body.addEventListener('keypress', () => this.reset());
+    window.addEventListener("load", () => this.reset());
+
+  }
+
+  private reset() {
+    console.log('date got by using events', Date.now());
+    this.setLastAction(Date.now());
+    console.log('store key', localStorage.getItem(this.STORE_KEY));
+
+  }
+
+  private initInterval() {
+    setInterval(() => {
+      this.check();
+    }, this.CHECK_INTERVAL);
+  }
+
+  private check() {
+    const now = Date.now();
+    const timeleft = this.getLastAction() + this.MINUTES_UNITL_AUTO_LOGOUT * 60 * 1000;
+    const diff = timeleft - now;
+    console.log('difference', diff)
+    const isTimeout = diff < 0;
+
+    if (isTimeout) {
+      localStorage.clear();
+      this.router.navigate(['/authentication/signin']);
+    }
+  }
+}
